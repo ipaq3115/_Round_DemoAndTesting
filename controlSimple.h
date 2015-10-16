45764d9a6f9fb9538f19eaf6f7b010f7f26ab930
Tim Bass <timsbass@gmail.com>
2015-10-16 11:51:22 -0400

    Fix control simple
    Now it's a north pointing dot

--- controlSimple.h
+++ controlSimple.h
@@ -11,15 +11,32 @@ image_info backgroundImageFile;
 int firstTouchIndex = -1;
 int currentQuadrant = -1;
 
+int oldHeading;
+
+bool dotMode;
+
+static int const BUFFER_SIZE = 50; 
+
+int16_t avgary[BUFFER_SIZE];
+int avgaryPtr;
+
 ControlSimplePage CONSTRUCTOR_MACRO
 
 void initalize() {
     
     watch->loadImage("controlS.raw",&backgroundImageFile);
     
-    watch->printImage(&backgroundImageFile,0,0,0);    
+    watch->setColor(BLACK);
+    watch->fillRect(0,0,219,219);
+    
+    // watch->printImage(&backgroundImageFile,0,0,0);    
     
     firstTouchIndex = -1;
+    dotMode = true;
+    oldHeading = -1;
+    
+    for(int i=0;i<BUFFER_SIZE;i++) avgary[i] = 0; 
+    avgaryPtr = 0;
     
 }
 
@@ -29,22 +46,90 @@ void leavingPage() {
 
 void loop() {
 
-    int newQuadrant = compass->heading() + 45;
-    while(newQuadrant < 0) newQuadrant += 360;
-    while(newQuadrant > 360) newQuadrant -= 360;
-    newQuadrant /= 90;
+    if(dotMode) {
     
-    if(newQuadrant != currentQuadrant) {
-    
-        currentQuadrant = newQuadrant;
+        int heading = compass->heading() * -1;
         
-        watch->printImage(&backgroundImageFile,0,0,currentQuadrant);
+        // bool stoponthis = false;
         
-    }
+        // if(oldHeading != -1 && abs(heading - oldHeading) > 180) stoponthis = true;
+        
+        avgaryPtr++;
+        if(avgaryPtr >= BUFFER_SIZE) avgaryPtr = 0;
+        avgary[avgaryPtr] = heading;
+        
+        int last = 0;
+        
+        heading = 0;
+        for(int i=0;i<BUFFER_SIZE;i++) {
+            
+            int tmp = avgary[i];
+            
+            if(tmp - last >= 180) tmp -= 360;
+            if(tmp - last <= -180) tmp += 360;
+            
+            // if(stoponthis) {
+            // 
+            //     db.printf("i %03d tmp %03d last %03d avgary[i] %03d\r\n", 
+            //     i, tmp, last, avgary[i]);
+            // 
+            // }
+            
+            heading += tmp;
+            last = tmp;
+            
+        }
+        heading /= BUFFER_SIZE;
+        
 
+        // if(stoponthis) {
+        // 
+        //     while(true);
+        // 
+        // }
+        
+        if(oldHeading != heading) {
+        
+            int x,y;
+        
+            watch->setColor(BLACK);
+            MyUtils::orbitPoint(110, 110, oldHeading, 90, x, y);
+            watch->fillCircle(x, y, 10);
+
+            watch->setColor(WHITE);
+            MyUtils::orbitPoint(110, 110, heading, 90, x, y);
+            watch->fillCircle(x, y, 10);
+            
+        }
+        
+        oldHeading = heading;
+    
+    } else {
+
+        int newQuadrant = compass->heading() + 45;
+        while(newQuadrant < 0) newQuadrant += 360;
+        while(newQuadrant > 360) newQuadrant -= 360;
+        newQuadrant /= 90;
+        
+        if(newQuadrant != currentQuadrant) {
+        
+            currentQuadrant = newQuadrant;
+        
+            watch->printImage(&backgroundImageFile,0,0,currentQuadrant);
+            
+        }
+
+    }
+    
 }
 
 void touch(int touchType,int finePos,int activeTouches,int touchIndex) {
+
+    if(touchType == RELEASED) {
+    
+        dotMode = true;
+    
+    }
 
 }
 
